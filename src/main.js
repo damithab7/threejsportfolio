4a4222c1fcb3d8659ce953701196d12a6eccdbfb
Damitha Sulochana <damitha.sb@gmail.com>
2026-01-02 14:17:13 +0530

    added a background and 200 stars randomly across the grid

--- src/main.js
+++ src/main.js
@@ -33,9 +33,24 @@ const gridHelper = new THREE.GridHelper(200,50);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-scene.add(lightHelper, gridHelper);
+// scene.add(lightHelper, gridHelper);
 
 scene.add(torus);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh( geometry, material);
+
+  const [x, y , z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y ,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
 
 function animate(){
   requestAnimationFrame( animate );
